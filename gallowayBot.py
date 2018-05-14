7b06921df4df8d0de50cda30095cd2210f10b8cb
John Stemberger <3868729+john-stemberger@users.noreply.github.com>
2018-05-13 20:46:30 -0400

    Galloway Sensei now has multiple classrooms
    Split functionality so GallowayBot can manage multiple independent slack channels (i.e. questions are independent of each other based on channel)

--- gallowayBot.py
+++ gallowayBot.py
@@ -4,9 +4,58 @@ import time
 import random
 from slackclient import SlackClient
 
+class Classroom:
+	def __init__(self):
+		self.questionIndex = 15
+		self.tries=0
+		self.refreshQuestion()
+
+	def processMessage(self, slack_client, channelName, message_text):
+		if re.match(r'quiz me', message_text, re.IGNORECASE):
+			self.refreshQuestion()
+			self.postNewQuestion(slack_client, channelName)
+		elif romanji[self.questionIndex].lower() == message_text.lower(): 
+			slack_client.api_call(
+				"chat.postMessage",
+				channel=channelName,
+				text="CORRECT!",
+				as_user=True)
+			self.refreshQuestion()
+			self.postNewQuestion(slack_client, channelName)
+		elif self.tries +1< MAX_TRIES:
+			self.tries = self.tries + 1
+			slack_client.api_call(
+				"chat.postMessage",
+				channel=channelName,
+				text="Not quite :( you have " + str(MAX_TRIES - self.tries) + " tries left",
+				as_user=True)
+			self.postNewQuestion(slack_client, channelName)
+		else:
+			slack_client.api_call(
+				"chat.postMessage",
+				channel=channelName,
+				text="Sorry, the correct answer is " + romanji[self.questionIndex],
+				as_user=True)
+			self.refreshQuestion()
+			self.postNewQuestion(slack_client, channelName)
+
+	def postNewQuestion(self, slack_client, channelName): 
+		question = "".join(["What is the sound that ", hiragana[self.questionIndex], " makes?"])
+		slack_client.api_call(
+			"chat.postMessage",
+			channel=channelName,
+			text=question,
+			as_user=True)
+	
+	def refreshQuestion(self): 
+		self.questionIndex = random.randint(0, len(hiragana)-1)
+		self.tries = 0;
+
+
 API_KEY_FILE = "slack_api_key.txt"
 
 MAX_TRIES = 3
+classrooms = {}
 questionIndex = 15
 tries=0
 
@@ -33,77 +82,37 @@ romanji=["a", "i", "u", "e", "o",
 "ra", "ri",  "ru",  "re", "ro", 
 "wa", "wo", "n"]
 
-def run() :
-	lines = [line.rstrip('\n') for line in open(API_KEY_FILE)]
-	slack_client = SlackClient(lines[0])
+lines = [line.rstrip('\n') for line in open(API_KEY_FILE)]
+slack_client = SlackClient(lines[0])
 
-	# Fetch your Bot's User ID
-	user_list = slack_client.api_call("users.list")
-	for user in user_list.get('members'):
-		if user.get('name') == "pibot":
-			slack_user_id = user.get('id')
-			break
+# Fetch your Bot's User ID
+user_list = slack_client.api_call("users.list")
+for user in user_list.get('members'):
+	if user.get('name') == "pibot":
+		slack_user_id = user.get('id')
+		break
 
-	# Start connection
-	if slack_client.rtm_connect():
-		print("Connected!")
-		for i in range(0, len(hiragana)):
-			print (hiragana[i] + " = " + romanji[i])
-		while True:
-			for message in slack_client.rtm_read():
-				# print "Message received: %s" % json.dumps(message, indent=2)
-				if 'text' in message and message['text'].startswith("<@%s>" % slack_user_id):
-					print ("Message received: %s" % json.dumps(message, indent=2))
-					message_text = message['text'].\
-						split("<@%s>" % slack_user_id)[1].\
-						strip()
-					processMessage(slack_client, message['channel'], message_text)
+# Start connection
+if slack_client.rtm_connect():
+	print("Connected!")
+	for i in range(0, len(hiragana)):
+		print (hiragana[i] + " = " + romanji[i])
+	while True:
+		for message in slack_client.rtm_read():
+			# print "Message received: %s" % json.dumps(message, indent=2)
+			if 'text' in message and message['text'].startswith("<@%s>" % slack_user_id):
+				print ("Message received: %s" % json.dumps(message, indent=2))
+				if message['channel'] in classrooms.keys():
+					print ("passing message to classroom")
+					room = classrooms[message['channel']]
+				else:
+					print ("creating new classroom")
+					room = Classroom()
+					classrooms[message['channel']] = room
 
-			time.sleep(1)
-			
-def processMessage(slack_client, channelName, message_text):
-	global tries
-	if re.match(r'quiz me', message_text, re.IGNORECASE):
-		refreshQuestion()
-		postNewQuestion(slack_client, channelName)
-	elif romanji[questionIndex].lower() == message_text.lower(): 
-		slack_client.api_call(
-			"chat.postMessage",
-			channel=channelName,
-			text="CORRECT!",
-			as_user=True)
-		refreshQuestion()
-		postNewQuestion(slack_client, channelName)
-	elif tries +1< MAX_TRIES:
-		tries = tries + 1
-		slack_client.api_call(
-			"chat.postMessage",
-			channel=channelName,
-			text="Not quite :( you have " + str(MAX_TRIES - tries) + " tries left",
-			as_user=True)
-	else:
-		slack_client.api_call(
-			"chat.postMessage",
-			channel=channelName,
-			text="Sorry, the correct answer is " + romanji[questionIndex],
-			as_user=True)
-		refreshQuestion()
-		postNewQuestion(slack_client, channelName)
+				message_text = message['text'].\
+					split("<@%s>" % slack_user_id)[1].\
+					strip()
+				room.processMessage(slack_client, message['channel'], message_text)
 
-def postNewQuestion(slack_client, channelName): 
-	question = "".join(["What is the sound that ", hiragana[questionIndex], " makes?"])
-	slack_client.api_call(
-		"chat.postMessage",
-		channel=channelName,
-		text=question,
-		as_user=True)
-	
-	
-def refreshQuestion(): 
-	global questionIndex
-	global tries
-	questionIndex = random.randint(0, len(hiragana)-1)
-	tries = 0;
-
-
-run()
+		time.sleep(1)
